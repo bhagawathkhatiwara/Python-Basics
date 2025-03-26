8cdb84f25e7c0d8409270a92a5577b8c301d91e6
bhagawathkhatiwara <bhagawathkhatiwara.jnec@rub.edu.bt>
2025-03-26 14:35:40 +0600

    added while loop

--- Basics.py
+++ Basics.py
@@ -6,3 +6,15 @@ elif num1 < num2:
     print(num1, "is less than", num2)
 else:
     print(num1, "is equal to", num2)
+    
+# The code above is a simple program that compares two numbers.
+
+num3 = int(input("enter number to print multiplication table: "))
+
+i=1
+while i<=num3:
+    product=num3*i
+    print(f"{num3} x {i} = {product}")
+    i += 1
+
+ # The code above is a simple program that prints the multiplication table of a number.
